d69f9e73c6b7461aae5347613c17ffa94f77da8d
Uchiha-itachi-01 <srk2004m@gmail.com>
2025-03-08 15:20:57 +0530

    Update start.py
    dd

--- plugins/start.py
+++ plugins/start.py
@@ -171,8 +171,8 @@ async def start_command(client: Client, message: Message):
             [
 
     [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton("⚡️ About", callback_data = "about"),
+                    InlineKeyboardButton('🍁 Developer', url='https://t.me/Uchiha_Developer')
 
     ]
             ]
